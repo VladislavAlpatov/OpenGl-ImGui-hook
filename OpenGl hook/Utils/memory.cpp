0c3c90485657ef1c52987d041307a5e1c8182d3d
Владислав Алпатов <alpatof74@gmail.com>
2022-07-18 01:10:58 +0300

    Add project files.

---
+++ OpenGl hook/Utils/memory.cpp
@@ -0,0 +1,78 @@
+#pragma once
+#include "memory.h"
+#include <sstream>
+#include <vector>
+#include <boost/algorithm/string.hpp>
+
+
+#include <Psapi.h>
+
+
+
+MODULEINFO GetModuleInfo(const char* szModule)
+{
+	MODULEINFO modinfo = { 0 };
+	HMODULE hModule = GetModuleHandleA(szModule);
+	if (hModule == NULL)
+		return modinfo;
+
+	GetModuleInformation(GetCurrentProcess(), hModule, &modinfo, sizeof(MODULEINFO));
+	return modinfo;
+}
+
+UINT StrHexToByte(const std::string& str)
+{
+	UINT iOutNumber;
+	std::stringstream ss;
+	ss << std::hex << str;
+	ss >> iOutNumber;
+	return iOutNumber;
+}
+
+std::vector<BYTE> GetSignatureBytes(const std::string& str)
+{
+
+	std::vector<std::string> strs;
+	std::vector<BYTE> bytes;
+	boost::split(strs, str, boost::is_any_of(" "));
+
+	for (auto& strHex : strs)
+	{
+		if (strHex == "??" or strHex == "?")
+		{
+			bytes.push_back('\?');
+			continue;
+		}
+
+		bytes.push_back(StrHexToByte(strHex));
+
+	}
+
+	return bytes;
+}
+
+uintptr_t Memory::FindPattern(const char* moduleName, const char* signature)
+{
+
+	MODULEINFO mInfo = GetModuleInfo(moduleName);
+	uintptr_t base = (uintptr_t)mInfo.lpBaseOfDll;
+
+	uintptr_t size = (uintptr_t)mInfo.SizeOfImage;
+
+	auto pattern = GetSignatureBytes(signature);
+
+	for (uintptr_t i = 0; i < size - pattern.size(); i++)
+	{
+		bool found = true;
+		for (uintptr_t j = 0; j < pattern.size(); j++)
+		{
+			found = pattern[j] == '\?' or pattern[j] == *(BYTE*)(base + i + j);
+			if (not found)
+				break;
+		}
+		if (found)
+			return base + i;
+	}
+
+	return NULL;
+}
---
+++ OpenGl hook/main.cpp
@@ -0,0 +1,77 @@
+﻿#include <Windows.h>
+#include <MinHook.h>
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_opengl3.h"
+#include "imgui/imgui_impl_win32.h"
+#include "Utils/memory.h"
+
+#include <gl/GL.h>
+
+
+LPVOID oWglSwapBuffers = NULL;
+
+
+
+__int64 __fastcall hWglSwapBuffers(HDC hdc)
+{
+    static bool imGuiInitialized = false;
+
+    if (!imGuiInitialized)
+    {
+        imGuiInitialized = true;
+        ImGui::CreateContext();
+        ImGui_ImplWin32_Init(WindowFromDC(hdc));
+        ImGui_ImplOpenGL3_Init();
+        ImGui::StyleColorsDark();
+
+    }
+
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplWin32_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::GetBackgroundDrawList()->AddText(ImVec2(), ImColor(255, 255, 255), u8"BRO YOU GOT HACKED!!!");
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+
+    typedef __int64(__fastcall* WglSwapBuffers_t)(HDC);
+    return reinterpret_cast<WglSwapBuffers_t>(oWglSwapBuffers)(hdc);
+}
+
+DWORD WINAPI HackThread(HMODULE hModule)
+{
+    MH_Initialize();
+
+    auto addr = (LPVOID)Memory::FindPattern("opengl32.dll", "48 89 5C 24 ? 48 89 74 24 ? 57 48 83 EC 40 48 8B F1 33 FF");
+
+    MH_CreateHook(addr, hWglSwapBuffers, &oWglSwapBuffers);
+    MH_EnableHook(MH_ALL_HOOKS);
+
+    while (!GetAsyncKeyState(VK_END))
+    {
+        Sleep(50);
+    }
+
+    MH_RemoveHook(MH_ALL_HOOKS);
+    MH_Uninitialize();
+    ImGui::DestroyContext();
+    
+
+    FreeLibraryAndExitThread(hModule, 0);
+}
+
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+        CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)HackThread, hModule, 0, nullptr);
+
+    return TRUE;
+}
+
